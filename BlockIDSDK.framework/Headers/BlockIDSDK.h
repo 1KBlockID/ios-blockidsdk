02f03ae0340c1769dc64021db83df986479d3ec6
Kuldeep Choudhary <kuldeep@1kosmos.com>
2021-07-26 18:02:55 +0530

    version 1.4.z.5 with Liveid improvements

--- BlockIDSDK.framework/Headers/BlockIDSDK.h
+++ BlockIDSDK.framework/Headers/BlockIDSDK.h
@@ -17,3 +17,5 @@ FOUNDATION_EXPORT const unsigned char ios_kernel_sdkVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <ios_kernel_sdk/PublicHeader.h>
 
 #import "MF_Base32Additions.h"
+
+#import "ShaderTypes.h"
