7d3a33926430403d7503efe5644dda2d500f974c
Sushil Tiwari <sushil.tiwari@1kosmos.com>
2022-03-12 16:05:36 +0530

    Initial Commit
    Initial Commit

---
+++ BlockIDSDK/Tests/BlockIDSDKTests/BlockIDSDKTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import BlockIDSDK
+
+final class BlockIDSDKTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(BlockIDSDK().text, "Hello, World!")
+    }
+}
