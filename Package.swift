e27ed6f6d75105d1521c9950ec863d3362bd455b
prasannaKosmos <82530183+prasannaKosmos@users.noreply.github.com>
2026-05-27 18:47:19 +0530

    MOB-7167-ios-sdk-dependency-upgrade-update-outdated-ios-libraries

--- Package.swift
+++ Package.swift
@@ -12,6 +12,28 @@ let package = Package(
             targets: ["BlockID"]
         )
     ],
+    dependencies: [
+        .package(
+            url: "https://github.com/krzyzanowskim/CryptoSwift.git",
+            exact: "1.10.0"
+        ),
+        .package(
+            url: "https://github.com/attaswift/BigInt.git",
+            exact: "5.7.0"
+        ),
+        .package(
+            url: "https://github.com/Alamofire/Alamofire.git",
+            exact: "5.11.2"
+        ),
+        .package(
+            url: "https://github.com/trustwallet/wallet-core.git",
+            exact: "4.6.9"
+        ),
+        .package(
+            url: "https://github.com/krzyzanowskim/OpenSSL-Package.git",
+            exact: "3.3.2000"
+        ),
+    ],
     targets: [
         .binaryTarget(
             name: "BlockID",
