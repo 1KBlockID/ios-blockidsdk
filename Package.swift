afa22bffa583c3b734098354ed77aa628987399c
prasannaKosmos <82530183+prasannaKosmos@users.noreply.github.com>
2026-05-28 11:44:18 +0530

    MOB-7167-ios-sdk-dependency-upgrade-update-outdated-ios-libraries

--- Package.swift
+++ Package.swift
@@ -9,42 +9,31 @@ let package = Package(
     products: [
         .library(
             name: "BlockID",
-            targets: ["BlockID"]
+            targets: ["BlockIDWrapper"]
         )
     ],
     dependencies: [
-        .package(
-            url: "https://github.com/krzyzanowskim/CryptoSwift.git",
-            exact: "1.10.0"
-        ),
-        .package(
-            url: "https://github.com/attaswift/BigInt.git",
-            exact: "5.7.0"
-        ),
-        .package(
-            url: "https://github.com/Alamofire/Alamofire.git",
-            exact: "5.11.2"
-        ),
-        .package(
-            url: "https://github.com/trustwallet/wallet-core.git",
-            exact: "4.6.9"
-        ),
-        .package(
-            url: "https://github.com/krzyzanowskim/OpenSSL-Package.git",
-            exact: "3.3.2000"
-        ),
+        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", exact: "1.10.0"),
+        .package(url: "https://github.com/attaswift/BigInt.git", exact: "5.7.0"),
+        .package(url: "https://github.com/Alamofire/Alamofire.git", exact: "5.11.2"),
+        .package(url: "https://github.com/trustwallet/wallet-core.git", exact: "4.6.9"),
+        .package(url: "https://github.com/krzyzanowskim/OpenSSL-Package.git", exact: "3.3.2000"),
     ],
     targets: [
         .binaryTarget(
             name: "BlockID",
+            path: "BlockID.xcframework"
+        ),
+        .target(
+            name: "BlockIDWrapper",
             dependencies: [
+                "BlockID",
                 "CryptoSwift",
                 "BigInt",
                 "Alamofire",
                 .product(name: "WalletCore", package: "wallet-core"),
                 .product(name: "OpenSSL", package: "OpenSSL-Package"),
             ]
-            path: "BlockID.xcframework"
         )
     ]
 )
