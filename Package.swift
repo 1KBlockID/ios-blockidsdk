0ed2e4a65790518960d715c00e07e226eeb9da65
prasannaKosmos <82530183+prasannaKosmos@users.noreply.github.com>
2026-06-02 23:45:20 +0530

    MOB-7167-ios-sdk-dependency-upgrade-update-outdated-ios-libraries

--- Package.swift
+++ Package.swift
@@ -18,7 +18,6 @@ let package = Package(
         .package(url: "https://github.com/Alamofire/Alamofire.git", exact: "5.11.2"),
         .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", exact: "1.10.0"),
         .package(url: "https://github.com/attaswift/BigInt.git", exact: "5.7.0"),
-        .package(url: "https://github.com/trustwallet/wallet-core.git", exact: "4.6.13"),
     ],
     targets: [
         // Wrapper target that links the binary xcframework with its dependencies.
@@ -32,7 +31,6 @@ let package = Package(
                 .product(name: "Alamofire", package: "Alamofire"),
                 .product(name: "CryptoSwift", package: "CryptoSwift"),
                 .product(name: "BigInt", package: "BigInt"),
-                .product(name: "WalletCore", package: "wallet-core"),
             ],
             path: "Sources/BlockIDTarget"
         ),
