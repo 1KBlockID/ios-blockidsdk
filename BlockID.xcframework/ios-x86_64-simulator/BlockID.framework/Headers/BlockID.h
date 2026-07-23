27c01f406c4b95030927ec99019dddaad85f1008
prasannaKosmos <82530183+prasannaKosmos@users.noreply.github.com>
2026-07-23 14:26:56 +0530

    mob-7293-1.30.50.rc01-code-sign

---
+++ BlockID.xcframework/ios-x86_64-simulator/BlockID.framework/Headers/BlockID.h
@@ -0,0 +1,21 @@
+//
+//  ios_kernel_sdk.h
+//  ios-kernel-sdk
+//
+//  Created by Kuldeep on 18/02/21.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+//! Project version number for ios_kernel_sdk.
+FOUNDATION_EXPORT double ios_kernel_sdkVersionNumber;
+
+//! Project version string for ios_kernel_sdk.
+FOUNDATION_EXPORT const unsigned char ios_kernel_sdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ios_kernel_sdk/PublicHeader.h>
+
+#import "MF_Base32Additions.h"
+
+#import "ShaderTypes.h"
